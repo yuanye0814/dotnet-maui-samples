9e837f3338d38986d9a3cb1bf0a119c39ca19584
Alvin Yuan <12433341+yuanye1006@user.noreply.gitee.com>
2023-04-27 14:19:06 +0800

    zh-CN, zh-Hans, zh-Hans-CN test

--- Fundamentals/LocalizationDemo/LocalizationDemo/MainPage.xaml.cs
+++ Fundamentals/LocalizationDemo/LocalizationDemo/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace LocalizationDemo;
+﻿using System.Globalization;
+
+namespace LocalizationDemo;
 
 public partial class MainPage : ContentPage
 {
@@ -19,6 +21,18 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+
+		string s1 = string.Format("Thread.CurrentThread.CurrentCulture:{0}\r\n", Thread.CurrentThread.CurrentUICulture);
+		string s2 = string.Format("Thread.CurrentThread.CurrentUICulture:{0}\r\n", Thread.CurrentThread.CurrentUICulture);
+		string s3 = string.Format("CultureInfo.DefaultThreadCurrentCulture:{0}\r\n", CultureInfo.DefaultThreadCurrentCulture);
+		string s4 = string.Format("CultureInfo.DefaultThreadCurrentUICulture:{0}\r\n", CultureInfo.DefaultThreadCurrentUICulture);
+		string s5 = string.Format("CultureInfo.CurrentCulture:{0}\r\n", CultureInfo.CurrentCulture);
+		string s6 = string.Format("CultureInfo.CurrentUICulture:{0}\r\n", CultureInfo.CurrentUICulture);
+
+		Shell.Current.DisplayAlert("cul",s1+s2+s3+s4+s5+s6,"OK");
+
+		Thread.CurrentThread.CurrentUICulture = new CultureInfo("zh-CN");
+
+    }
 }
 
